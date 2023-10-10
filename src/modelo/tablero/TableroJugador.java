6ecf3dd00d9718b5c47f96064f052348a2104c1f
MateoPonti <63692136+MateoPonti@users.noreply.github.com>
2023-10-10 17:53:23 -0300

    Haciendo logica del tablero

--- src/modelo/tablero/TableroJugador.java
+++ src/modelo/tablero/TableroJugador.java
@@ -2,6 +2,7 @@ package modelo.tablero;
 
 import modelo.contenedor.Agujero;
 import modelo.contenedor.Contenedor;
+import modelo.contenedor.TipoContenedor;
 import modelo.contenedor.Zona;
 import modelo.haba.IHaba;
 
@@ -11,6 +12,13 @@ public class TableroJugador {
 
     private final int cantidadAgujeros=6;
     private  Contenedor[] tablero;
+
+    private boolean ultimaCayoVacio=false;
+
+    private boolean ultimaCayoZona= false;
+
+
+
 
     public TableroJugador(){
         inicializar();
@@ -26,27 +34,44 @@ public class TableroJugador {
 
 
     public ArrayList<IHaba> repartirHabasTurno(int posicion){
+        ultimaCayoVacio=false;
+        ultimaCayoZona=false;
+
         Contenedor contenedor=  tablero[posicion];
         ArrayList<IHaba> habas= contenedor.sacarHabas();
-        while ((posicion<=7) && (!habas.isEmpty())){
-            tablero[posicion].agregar( habas.get(habas.size()-1));
+
+
+        posicion++;
+        while ((posicion<=6) && (!habas.isEmpty())){
+            Contenedor contActual=tablero[posicion];
+            int tam= habas.size();
+            ultimaCayoVacio=tam==1  && contActual.estaVacio() && contActual.getTipo()== TipoContenedor.Agujero;
+            ultimaCayoZona= tam==1  && contActual.getTipo()== TipoContenedor.Zona;
+            contActual.agregar( habas.get(tam-1));
             posicion++;
-            habas.remove(habas.size()-1);
+            habas.remove(tam-1);
         }
         return habas;
     }
 
     public ArrayList<IHaba> repartirHabasOponente(ArrayList<IHaba> habas){
-        int posicion=6;
-        while ((posicion>0) && (!habas.isEmpty())){
-            tablero[posicion].agregar( habas.get(habas.size()-1));
+        int posicion=5;
+        while ((posicion>=0) && (!habas.isEmpty())){
+            int tam= habas.size();
+            tablero[posicion].agregar( habas.get(tam-1));
             posicion--;
-            habas.remove(habas.size()-1);
+            habas.remove(tam-1);
         }
         return habas;
     }
 
 
+    public boolean isUltimaCayoVacio() {
 
+        return ultimaCayoVacio;
+    }
 
+    public boolean isUltimaCayoZona() {
+        return ultimaCayoZona;
+    }
 }
